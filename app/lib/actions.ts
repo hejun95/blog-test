b01cd6158cb5017c07ab3c2556b91779ee3fabef
honghejun <1284023786@qq.com>
2024-09-04 21:21:48 +0800

    fix: 首页和发票页面开发

---
+++ app/lib/actions.ts
@@ -0,0 +1,81 @@
+'use server';
+
+import { z } from 'zod';
+import { sql } from '@vercel/postgres'; // 这里需要注意
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(['pending', 'paid']),
+  date: z.string(),
+});
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+
+export async function createInvoice(formData: FormData) {
+  const { customerId, amount, status } = CreateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split('T')[0];
+
+  try {
+    await sql`
+      INSERT INTO invoices (customer_id, amount, status, date)
+      VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+    `;
+  } catch (Error) {
+    return {
+      message: '数据库错误：新建发票失败！ ',
+    };
+  }
+  revalidatePath('/dashboard/invoices');// 清除客户端路由缓存并触发对服务器的新请
+  redirect('/dashboard/invoices');// 重定向到 /dashboard/invoices 路由
+}
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+
+  const amountInCents = amount * 100;
+
+  try {
+    await sql`
+      UPDATE invoices
+      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+      WHERE id = ${id}
+    `;
+  } catch (Error) {
+    return {
+      message: '数据库错误：修改发票失败！ ',
+    };
+  }
+
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
+
+export async function deleteInvoice(id: string) {
+  throw new Error('Failed to Delete Invoice');
+  try {
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+    revalidatePath('/dashboard/invoices');
+    return {
+      message: '成功删除发票 ',
+    };
+  } catch (error) {
+    return {
+      message: '数据库错误：修改发票失败！ ',
+    };
+  }
+}
